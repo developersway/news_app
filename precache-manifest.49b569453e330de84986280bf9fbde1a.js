56258d3b4e34b491df8d910569e19ef155062cf6
developersway <aman.ak2012@gmail.com>
2019-10-25 20:21:01 +0530

    Updates

---
+++ precache-manifest.49b569453e330de84986280bf9fbde1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bd8d08f96e0706cdc92acaff7b1dff",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "e3747753626aef222b81",
+    "url": "/news_app/static/css/main.a6ea49d9.chunk.css"
+  },
+  {
+    "revision": "b5d333657df42aed0f2b",
+    "url": "/news_app/static/js/2.029f0bb2.chunk.js"
+  },
+  {
+    "revision": "e3747753626aef222b81",
+    "url": "/news_app/static/js/main.becad22d.chunk.js"
+  },
+  {
+    "revision": "c11226c7493391be0e82",
+    "url": "/news_app/static/js/runtime-main.540d29a5.js"
+  }
+]);
